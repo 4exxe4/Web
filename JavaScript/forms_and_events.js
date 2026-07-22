92a86638feae5fca48666ec90a94bd2cc1d7944d
user <user@WIN-CP209TC39O5>
2026-07-22 21:06:05 +0300

    Color event DONE

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -11,3 +11,26 @@ function Factorial()
     }
     resultElement.innerHTML = `${number}! = ${f}`;
 }
+
+function setImage()
+{
+    let filename = document.getElementById("image-file");
+    let reader = new FileReader();
+    reader.onload = function (e)
+    {
+        document.getElementById("image").src = e.target.result;
+    }
+    reader.readAsDataURL(filename.files[0]);
+}
+function SetBackgroundColor(e)
+{
+    document.body.style.backgroundColor = e.target.value;
+    //document.body.style.backgroundColor = document.getElementById("backround-color").value;
+    //let color = document.getElementById("background-color").value;
+    //document.body.style.backgroundColor = color;
+    //alert("SetBackGroundColor");
+}
+function setForegroundColor()
+{
+    document.body.style.color = document.getElementById("foreground-color").value;
+}
