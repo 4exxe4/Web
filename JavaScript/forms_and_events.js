d9e39b6f6f84c615c8c85f4ba3dbc48b0d9fd849
user <user@WIN-CP209TC39O5>
2026-07-27 17:35:15 +0300

    Switch background DONE. need optimisation

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -16,21 +16,58 @@ function setImage()
 {
     let filename = document.getElementById("image-file");
     let reader = new FileReader();
-    reader.onload = function (e)
-    {
+    reader.onload = function (e) {
         document.getElementById("image").src = e.target.result;
     }
     reader.readAsDataURL(filename.files[0]);
 }
-function SetBackgroundColor(e)
+function setBackgroundColor(event)
 {
-    document.body.style.backgroundColor = e.target.value;
-    //document.body.style.backgroundColor = document.getElementById("backround-color").value;
-    //let color = document.getElementById("background-color").value;
-    //document.body.style.backgroundColor = color;
-    //alert("SetBackGroundColor");
+    document.body.style.backgroundColor = event.target.value;
+    console.log(event.target.id);
+    //document.body.style.backgroundColor = document.getElementById("background-color").value;
+    /*let color = document.getElementById("background-color").value;
+    document.body.style.backgroundColor = color;
+    alert("setBackgroundColor");*/
 }
 function setForegroundColor()
 {
     document.body.style.color = document.getElementById("foreground-color").value;
+}
+let colors = document.getElementById("foreground-color");
+colors.addEventListener("input", setColor);
+function setColor(event)
+{
+    /*
+    --------------------------------
+    ==  - сравнивает два значения
+    === - сравнивает два значения и типы этих значеий
+    ===   возвращает 'true' только в том случае, если совпадают как значения, так и типы
+    --------------------------------
+   //*/
+    document.body.style[(event.target.id === 'background-color' ? 'backgroundColor' : 'color')] = event.target.value;
+   // if (event.target.id === 'background-color')
+   //     document.body.style.backgroundColor = event.target.value;
+   // else
+   //     document.body.style.color = event.target.value;
+    console.log(event.target.id);
+    //alert("setColor");
+}
+document.addEventListener("mousemove", traceMouse);
+function traceMouse(e)
+{
+    document.getElementById("mouse").innerHTML =
+        `X = ${e.clientX}, Y = ${e.clientY}`;
+}
+
+document.getElementById("switch-background").addEventListener("click", switchBackground);
+function switchBackground(e)
+{
+    let skin = document.body.className;
+    let switchButton = document.getElementById("switch-background");
+    switchButton.src = skin === "dark" ? "moon.png" : "sun.png";
+    document.body.className = skin === "dark" ? "light" : "dark";
+    //document.getElementById("debug-background").innerHTML = switchButton.src;
+    document.getElementById("debug-background").innerHTML = document.body.className;
+
 }
