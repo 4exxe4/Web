d56890ad6da71e02241eb03b6847151b190f7b8c
user <user@WIN-CP209TC39O5>
2026-07-27 19:59:27 +0300

    Switch background delay DONE

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -72,3 +72,12 @@ function switchBackground(e)
     //document.getElementById("debug-background").innerHTML = document.body.className;
 
 }
+document.getElementById("switch-background-delay").addEventListener("change", setDelay);
+function setDelay(e)
+{
+    let delay = e.target.value;
+    document.getElementById(`switch-background`).style.transition = document.body.style.transition =
+        `color ${delay}s, background-color ${delay}s, background-image ${delay}s`;
+    console.log(document.body.style);
+    console.log(document.getElementById('#switch-background').style);
+}
